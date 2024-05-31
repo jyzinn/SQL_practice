e9230d3feb5e9696b028dcf755145cd6cf55c695
jyzinn <yoojinjeon97@gmail.com>
2024-05-31 17:18:42 +0900

    Test modification to SQL_practice.sql

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE sql_practice;
 USE	sql_practice;
 
+/* 
+solvesql 레스토랑의 일일 매출
+https://solvesql.com/problems/daily-revenue/ 
+*/
+
+SELECT  day,
+        SUM(total_bill) AS revenue_daily
+FROM    tips
+GROUP BY day
+HAVING  SUM(total_bill) >= 1000
+ORDER BY revenue_daily DESC;
