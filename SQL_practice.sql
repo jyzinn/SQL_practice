92a42595259c0bee090b128a035d86d8b1cd2b23
jyzinn <yoojinjeon97@gmail.com>
2024-05-31 17:47:31 +0900

    solvesql 버뮤다 삼각지대에 들어가버린 택배

--- SQL_practice.sql
+++ SQL_practice.sql
@@ -12,3 +12,16 @@ FROM    tips
 GROUP BY day
 HAVING  SUM(total_bill) >= 1000
 ORDER BY revenue_daily DESC;
+
+/* 
+solvesql 버뮤다 삼각지대에 들어가버린 택배
+https://solvesql.com/problems/shipment-in-bermuda/
+*/
+
+SELECT  DATE(order_delivered_carrier_date) AS delivered_carrier_date,
+        COUNT(*) AS orders
+FROM    olist_orders_dataset
+WHERE   order_delivered_carrier_date LIKE '2017-01%'
+        AND order_delivered_customer_date IS NULL
+GROUP BY delivered_carrier_date
+ORDER BY 1;
