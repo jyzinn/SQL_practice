254fa30b6e00e41bdda2f0312bd6342a6c728b16
jyzinn <yoojinjeon97@gmail.com>
2024-07-07 11:56:56 +0900

    흉부외과 또는 일반외과 의사 목록 출력하기

--- 프로그래머스_SQL고득점kit.sql
+++ 프로그래머스_SQL고득점kit.sql
@@ -49,3 +49,18 @@ WHERE   MONTH(date_of_birth) = 3
         AND gender = 'w'
         AND tlno IS NOT NULL
 ORDER BY member_id;
+
+/*
+흉부외과 또는 일반외과 의사 목록 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/132203
+*/
+
+SELECT  dr_name,
+        dr_id,
+        mcdp_cd,
+        -- 출력 요구 형식에 맞추어 포맷팅
+        DATE_FORMAT(hire_ymd, '%Y-%m-%d') AS hire_ymd
+FROM    doctor
+		-- 진료과가 흉부외과(CS)이거나 일반외과(GS)인 의사만 조회
+WHERE   mcdp_cd IN ('CS', 'GS')
+ORDER BY hire_ymd DESC, dr_name;
