5705693073ca1d9f6c1771b3ce5813fbc9b8a213
jyzinn <yoojinjeon97@gmail.com>
2024-07-07 11:45:59 +0900

    조건에 부합하는 중고거래 댓글 조회하기

--- 프로그래머스_SQL고득점kit.sql
+++ 프로그래머스_SQL고득점kit.sql
@@ -12,3 +12,23 @@ WHERE   published_date LIKE '2021%'
         AND category = '인문'
 		-- pusblised_date로 오름차순 정렬
 ORDER BY published_date;
+
+/*
+조건에 부합하는 중고거래 댓글 조회하기
+https://school.programmers.co.kr/learn/courses/30/lessons/164673
+*/
+
+SELECT  A.title,
+        A.board_id,
+        B.reply_id,
+        B.writer_id,
+        B.contents,
+        -- 출력 요구 형식에 맞추어 포맷팅
+        DATE_FORMAT(B.created_date, '%Y-%m-%d') AS created_date
+FROM    used_goods_board AS A
+		-- 게시글 ID를 기준으로 JOIN
+INNER JOIN used_goods_reply AS B
+ON      A.board_id = B.board_id
+		-- 2022년 10월에 작성된 게시글만 조회
+WHERE   A.created_date LIKE '2022-10%'
+ORDER BY B.created_date, A.title;
