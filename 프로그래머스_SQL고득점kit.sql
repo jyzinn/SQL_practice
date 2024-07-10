9b76b31f0ecff6996cb77692f523a47d5b43ad01
jyzinn <yoojinjeon97@gmail.com>
2024-07-10 17:18:11 +0900

    조건에 맞는 회원수 구하기

--- 프로그래머스_SQL고득점kit.sql
+++ 프로그래머스_SQL고득점kit.sql
@@ -243,3 +243,12 @@ SELECT	name
 FROM 	animal_ins
 ORDER BY datetime
 LIMIT 	1;
+
+/*
+조건에 맞는 회원수 구하기
+https://school.programmers.co.kr/learn/courses/30/lessons/131535
+*/
+SELECT  COUNT(user_id) AS users
+FROM    user_info
+WHERE   joined LIKE '2021%'
+        AND age BETWEEN 20 AND 29;
