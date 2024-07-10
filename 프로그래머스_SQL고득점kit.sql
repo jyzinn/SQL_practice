28732773eca6f3a3518bb14cdcc706912977036d
jyzinn <yoojinjeon97@gmail.com>
2024-07-10 15:49:16 +0900

    어린 동물 찾기

--- 프로그래머스_SQL고득점kit.sql
+++ 프로그래머스_SQL고득점kit.sql
@@ -198,3 +198,13 @@ SELECT  animal_id,
 FROM    animal_ins
 WHERE   intake_condition = 'Sick'
 ORDER BY animal_id;
+
+/*
+어린 동물 찾기
+https://school.programmers.co.kr/learn/courses/30/lessons/59037
+*/
+SELECT  animal_id,
+        name
+FROM    animal_ins
+WHERE   intake_condition != 'Aged'
+ORDER BY animal_id;
