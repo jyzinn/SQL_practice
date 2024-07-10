8a1d3c12e9276c0ae59522cc6fa00ae7a38f0e01
jyzinn <yoojinjeon97@gmail.com>
2024-07-10 16:17:20 +0900

    동물의 아이디와 이름

--- 프로그래머스_SQL고득점kit.sql
+++ 프로그래머스_SQL고득점kit.sql
@@ -208,3 +208,12 @@ SELECT  animal_id,
 FROM    animal_ins
 WHERE   intake_condition != 'Aged'
 ORDER BY animal_id;
+
+/*
+동물의 아이디와 이름
+https://school.programmers.co.kr/learn/courses/30/lessons/59403
+*/
+SELECT  animal_id,
+        name
+FROM    animal_ins
+ORDER BY animal_id;
