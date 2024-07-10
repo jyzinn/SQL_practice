cc4faa1e2177ad1809001ec9f71a1e77cc131cbc
jyzinn <yoojinjeon97@gmail.com>
2024-07-10 16:25:06 +0900

    여러 기준으로 정렬하기

--- 프로그래머스_SQL고득점kit.sql
+++ 프로그래머스_SQL고득점kit.sql
@@ -217,3 +217,13 @@ SELECT  animal_id,
         name
 FROM    animal_ins
 ORDER BY animal_id;
+
+/*
+여러 기준으로 정렬하기
+https://school.programmers.co.kr/learn/courses/30/lessons/59404
+*/
+SELECT  animal_id,
+        name,
+        datetime
+FROM    animal_ins
+ORDER BY name, datetime DESC;
